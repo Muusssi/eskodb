bdb392b43615479bc99456da5db8fd5eb6da039b
Tommi Oinonen <tommi.oinonen@gmail.com>
2018-05-12 21:35:01 +0300

    fixed player lists and removed xsfr checking

--- tornado_serveri.py
+++ tornado_serveri.py
@@ -59,7 +59,7 @@ class Application(tornado.web.Application):
                 template_path=TEMPLATES_DIRECTORY,
                 static_path=STATIC_DIRECTORY,
                 cookie_secret=config['cookie'],
-                xsrf_cookies=True,
+                xsrf_cookies=False,
                 login_url="/login",
                 debug=True,
             )
