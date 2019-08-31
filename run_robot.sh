1c4c3664cfadb0f87e8967d113e18a47cbe23e1c
Tommi Oinonen <tommi.oinonen@gmail.com>
2019-08-31 18:22:10 +0300

    updates tests to use python3

--- run_robot.sh
+++ run_robot.sh
@@ -4,7 +4,7 @@ psql -U esko -c "DROP DATABASE test_eskodb;"
 psql -U esko -c "CREATE DATABASE test_eskodb;"
 psql test_eskodb -U esko -f eskodb2.sql
 
-PYTHONPATH=./ python tornado_serveri.py tests/test_config.json &
+PYTHONPATH=./ python3 tornado_serveri.py tests/test_config.json &
 SERVER_PID=$!
 
 robot --outputdir=tests/functional/results \
